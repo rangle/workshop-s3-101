6d0744c04187ec868755e7bf3d673388d702aa64
Meng Fu <meng.fu@rangle.io>
2018-09-25 09:43:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Site
+
+To build the application, run:
+
+```sh
+npm install -g yarn
+yarn install
+yarn build
+```
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Welcome">
+          Welcome to My React App!
+        </div>
+        <div className="Name">
+          [Your name]
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
